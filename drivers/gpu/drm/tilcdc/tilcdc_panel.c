c9dfc5cb0aff219b35e3b31babee963fe84805a1
xinyun <rongdong@embest-tech.com>
2016-07-16 11:48:15 +0800

    Backlight:code: Add gpio backlight support for AM335x
    Add gpio backlight support for AM335x
    - you can use only gpio to control the backlight automatically by LCD
    - you can use only PWM to control the backlight automatically by LCD or manually by user
    - you can use gpio to control the backlight automatically by LCD and use PWM  to control the backlight manually by user
    
    At the third condition,you need to modify the code accordding to your board
    
    Signed-off-by: xinyun <rongdong@embest-tech.com>

--- drivers/gpu/drm/tilcdc/tilcdc_panel.c
+++ drivers/gpu/drm/tilcdc/tilcdc_panel.c
@@ -58,7 +58,18 @@ static void panel_encoder_dpms(struct drm_encoder *encoder, int mode)
 	struct panel_encoder *panel_encoder = to_panel_encoder(encoder);
 	struct backlight_device *backlight = panel_encoder->mod->backlight;
 	struct gpio_desc *gpio = panel_encoder->mod->enable_gpio;
+	void __iomem *reg;
 
+	if (gpio) {
+		if (backlight) {
+			reg = ioremap(0x44E10800, 0x200);
+			reg += 0x164;
+			if (mode == DRM_MODE_DPMS_ON)
+				writel(0, reg);
+			else
+				writel(7, reg);
+		}
+	}
 	if (backlight) {
 		backlight->props.power = mode == DRM_MODE_DPMS_ON ?
 					 FB_BLANK_UNBLANK : FB_BLANK_POWERDOWN;
